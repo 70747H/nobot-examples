67ea44b82ec01b9328f51fb323e63b5b7b1a37c0
Hussien <hussien@sharpmobile.ca>
2018-09-30 16:23:31 +0200

    003 querystring example

--- examples/003/parse-querystring.js
+++ examples/003/parse-querystring.js
@@ -1,1 +1,8 @@
 const querystring = require('querystring');
+
+var url = 'http://www.opencanvas.co.uk?myName=Shaun&myAge=28&comment=Yes+I+am+getting+old';
+var parsedUrl = querystring.parse(url.substring(url.indexOf('?') + 1));
+
+console.log(`Hi	my	name	is	${parsedUrl.myName}`);
+console.log(`I	am	${parsedUrl.myAge}`);
+console.log(`Oh	and...	${parsedUrl.comment}`);
